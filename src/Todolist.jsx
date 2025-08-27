86c48583da63ec704ecb2f3d1be7a884cfb5dd50
varunjoshi27 <varunjoshi1127@gmail.com>
2025-08-27 22:48:15 +0530

    User Input Updated

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -2,24 +2,35 @@ import { useState } from 'react';
 
 export default function Todolist() {
   let [todo, settodo] = useState([]);
+  let [newtodo, setnewtodo] = useState('');
 
-  let updatetask = () => {
-    settodo(todo);
+  let addnewtask = () => {
+    settodo([...todo, newtodo]);
+    setnewtodo('');
+  };
+
+  let updatetodovalue = (event) => {
+    setnewtodo(event.target.value);
   };
   return (
     <>
       <div>
-        <input type="text" placeholder="Enter the task" />
+        <input
+          type="text"
+          placeholder="Enter the task"
+          value={newtodo}
+          onChange={updatetodovalue}
+        />
         <br />
         <br />
-        <button onClick={updatetask}>Add Task</button>
+        <button onClick={addnewtask}>Add Task</button>
         <br />
         <br />
         <hr />
         <h4>Todo List</h4>
         <ul>
           {todo.map((todo) => {
-            <li>{todo}</li>;
+            return <li>{todo}</li>;
           })}
         </ul>
       </div>
